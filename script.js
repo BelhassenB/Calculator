3aa8747cd641f9578259e209c385ed8e8fc770dd
Belhassen <152809495+BelhassenB@users.noreply.github.com>
2024-03-11 11:56:01 +0100

    Make the display accept/show two numbers

--- script.js
+++ script.js
@@ -1,82 +1,105 @@
 const numBtns = document.querySelectorAll('.num')
 const displayInp = document.querySelector('.input-result')
 const operatorBtns = document.querySelectorAll('.operator')
-
 const clearAll = document.querySelector('.all-clear')
 const equalBtns = document.querySelector('.equal')
+const clearLastChar = document.querySelector('.clear-previous')
 
 
-let display = "";
+let displayValue = ''
+let num1 = ''
+let num2 = ''
+let operator = ''
 
 clearAll.addEventListener('click', () => {
     displayInp.textContent = ""
     operator = ""
-    num1;
-    num2;
+    num1 = ""
+    num2 = ""
+})
+
+clearLastChar.addEventListener('click', () => {
+    displayValue = displayInp.textContent
+    lastChar = displayValue.length-1
+
+    if (displayValue !== '')  {
+        displayInp.textContent = displayValue.slice(0, lastChar)
+    }
+
 })
 
 function add(...args) {
-    return displayInp.textContent = args.reduce((total, item) => total + item)
+    return displayInp.textContent = 
+    args.reduce((total, item) => total + item)
     
 
 }
 
 function substract(...args) {
-    return displayInp.textContent = args.reduce((total, item) => total - item)
+    return displayInp.textContent =
+    args.reduce((total, item) => total - item)
 }
 
 
 function multiply(...args) {
-    return displayInp.textContent = args.reduce((total, item) => total * item)
+    return displayInp.textContent =
+    args.reduce((total, item) => total * item)
 }
 
 
 function divide(...args) {
-    return displayInp.textContent = args.reduce((total, item) => total/item)
+    return displayInp.textContent =
+    args.reduce((total, item) => total/item)
+
+    
 }
 
 
-let num1;
-let num2;
-let operator;
-
-
-
     numBtns.forEach(num => {
-        num.addEventListener('click', (e) => {
-        displayInp.textContent += e.target.textContent   
-        display = displayInp.textContent
-                   
-        
+        num.addEventListener('click', (e) => { 
+            
+        num2 += e.target.textContent  
+        displayInp.textContent += e.target.textContent
+
     })})      
 
     operatorBtns.forEach(op => {
-        op.addEventListener('click', (e) => { 
-            operator = e.target.textContent           
-            displayInp.textContent += e.target.textContent   
-            display = displayInp.textContent
+        op.addEventListener('click', (e) => {             
 
-            
+            if (num1 === '') {
+                num1 = num2
+                num2 = ''
+                operator = e.target.textContent
+                displayInp.textContent = num1 + operator + num2 
+            } else if (num1 !== '' && num2 !== '' && operator !== '') {                
 
-            console.log(e.target.classList)            
+                operate(parseFloat(num1),operator,parseFloat(num2))
+                num1 = displayInp.textContent
+                num2 = ''
+                operator = ''
+                operator = e.target.textContent
+                displayInp.textContent = num1 + operator + num2
                  
+            } else {
+                operator = e.target.textContent
+                displayInp.textContent += e.target.textContent
+                
+            }
 
-    })})
-
-    
-
-        equalBtns.addEventListener('click', () => {
-            result = display.split(operator)
-            num1 = parseInt(result[0])
-            num2 = parseInt(result[1])
-    
-            console.log(operate(num1, operator, num2))
-    
             
-        })
 
 
-    
+    })})   
+
+    equalBtns.addEventListener('click', () => {  
+        operate(parseFloat(num1),operator,parseFloat(num2))  
+        num1 = displayInp.textContent
+        num2 = ''
+        operator = ''   
+            
+
+     })   
+
 
     function operate (firstNum,symbol,secondNum) {
        
@@ -93,9 +116,12 @@ let operator;
                 break;
             case '/':
                 divide(firstNum, secondNum)
+                if (secondNum == 0 ) displayInp.textContent = "ERROR"
+                
                 break;        
         }
 
+        
 
     }
 
